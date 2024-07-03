4ba0d6d16b6690926e130dcdf87ca0d3e427eefb
Giselle <gisellevonbingen@gmail.com>
2024-07-03 10:18:27 +0900

    Delete ViewOverrideExtension.java

--- src/main/java/steve_gall/minecolonies_compatibility/core/client/gui/ViewOverrideExtension.java
+++
@@ -1,9 +0,0 @@
-package steve_gall.minecolonies_compatibility.core.client.gui;
-
-import com.ldtteam.blockui.PaneParams;
-import com.ldtteam.blockui.views.View;
-
-public interface ViewOverrideExtension
-{
-	void minecolonies_compatibility$onParse(View view, PaneParams params);
-}
