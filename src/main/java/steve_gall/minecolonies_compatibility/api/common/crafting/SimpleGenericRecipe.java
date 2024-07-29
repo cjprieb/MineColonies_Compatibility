4092ca363d814a1e1904be4d17f4feaa260cc8e2
Giselle <gisellevonbingen@gmail.com>
2024-07-29 16:17:32 +0900

    Simplified

---
+++ src/main/java/steve_gall/minecolonies_compatibility/api/common/crafting/SimpleGenericRecipe.java
@@ -0,0 +1,135 @@
+package steve_gall.minecolonies_compatibility.api.common.crafting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import com.minecolonies.api.crafting.IGenericRecipe;
+import com.minecolonies.api.util.OptionalPredicate;
+import com.minecolonies.api.util.constant.IToolType;
+import com.minecolonies.api.util.constant.ToolType;
+
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import steve_gall.minecolonies_compatibility.core.common.crafting.GenericRecipeHelper;
+import steve_gall.minecolonies_compatibility.core.common.crafting.IngredientHelper;
+
+public class SimpleGenericRecipe implements IGenericRecipe
+{
+	@NotNull
+	private final ResourceLocation recipeId;
+	@NotNull
+	private final List<List<ItemStack>> input;
+	@NotNull
+	private final ItemStack output;
+	@NotNull
+	private final List<ItemStack> additionalOutputs;
+
+	public SimpleGenericRecipe(@NotNull Recipe<?> recipe, @NotNull RegistryAccess registryAccess)
+	{
+		this(recipe.getId(), IngredientHelper.getStacksList(recipe.getIngredients()), recipe.getResultItem(registryAccess));
+	}
+
+	public SimpleGenericRecipe(@NotNull ResourceLocation recipeId, @NotNull List<List<ItemStack>> ingredients, @NotNull ItemStack output)
+	{
+		this.recipeId = recipeId;
+		this.input = new ArrayList<>(ingredients);
+		this.output = output;
+		this.additionalOutputs = GenericRecipeHelper.getAdditionalOutputs(ingredients);
+	}
+
+	@Override
+	public int getGridSize()
+	{
+		return 1;
+	}
+
+	@Override
+	public @Nullable ResourceLocation getRecipeId()
+	{
+		return this.recipeId;
+	}
+
+	@Override
+	public @NotNull ItemStack getPrimaryOutput()
+	{
+		return this.output;
+	}
+
+	@Override
+	public @NotNull List<ItemStack> getAllMultiOutputs()
+	{
+		return Collections.singletonList(this.output);
+	}
+
+	@Override
+	public @NotNull List<ItemStack> getAdditionalOutputs()
+	{
+		return this.additionalOutputs;
+	}
+
+	@Override
+	public @NotNull List<List<ItemStack>> getInputs()
+	{
+		return this.input;
+	}
+
+	@Override
+	public Optional<Boolean> matchesOutput(@NotNull OptionalPredicate<ItemStack> predicate)
+	{
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Boolean> matchesInput(@NotNull OptionalPredicate<ItemStack> predicate)
+	{
+		return Optional.empty();
+	}
+
+	@Override
+	public @NotNull Block getIntermediate()
+	{
+		return Blocks.AIR;
+	}
+
+	@Override
+	public @Nullable ResourceLocation getLootTable()
+	{
+		return null;
+	}
+
+	@Override
+	public @NotNull IToolType getRequiredTool()
+	{
+		return ToolType.NONE;
+	}
+
+	@Override
+	public @Nullable LivingEntity getRequiredEntity()
+	{
+		return null;
+	}
+
+	@Override
+	public @NotNull List<Component> getRestrictions()
+	{
+		return Collections.emptyList();
+	}
+
+	@Override
+	public int getLevelSort()
+	{
+		return -1;
+	}
+
+}
--- src/main/java/steve_gall/minecolonies_compatibility/module/common/farmersdelight/init/ModuleCraftingTypes.java
+++ src/main/java/steve_gall/minecolonies_compatibility/module/common/farmersdelight/init/ModuleCraftingTypes.java
@@ -4,16 +4,18 @@ import com.minecolonies.api.crafting.registry.CraftingType;
 
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
+import steve_gall.minecolonies_compatibility.api.common.crafting.SimpleCraftingType;
 import steve_gall.minecolonies_compatibility.core.common.MineColoniesCompatibility;
-import steve_gall.minecolonies_compatibility.module.common.farmersdelight.crafting.CookingCraftingType;
+import steve_gall.minecolonies_compatibility.module.common.farmersdelight.crafting.CookingGenericRecipe;
 import steve_gall.minecolonies_compatibility.module.common.farmersdelight.crafting.CuttingCraftingType;
 import steve_gall.minecolonies_tweaks.api.registries.DeferredRegisterHelper;
+import vectorwing.farmersdelight.common.registry.ModRecipeTypes;
 
 public class ModuleCraftingTypes
 {
 	public static final DeferredRegister<CraftingType> REGISTER = DeferredRegisterHelper.craftingTypes(MineColoniesCompatibility.MOD_ID);
-	public static final RegistryObject<CuttingCraftingType> CUTTING = DeferredRegisterHelper.registerCraftingType(REGISTER, "farmers_cutting", CuttingCraftingType::new);
-	public static final RegistryObject<CookingCraftingType> COOKING = DeferredRegisterHelper.registerCraftingType(REGISTER, "farmers_cooking", CookingCraftingType::new);
+	public static final RegistryObject<CraftingType> CUTTING = DeferredRegisterHelper.registerCraftingType(REGISTER, "farmers_cutting", id -> new SimpleCraftingType<>(id, ModRecipeTypes.COOKING, CookingGenericRecipe::new));
+	public static final RegistryObject<CraftingType> COOKING = DeferredRegisterHelper.registerCraftingType(REGISTER, "farmers_cooking", CuttingCraftingType::new);
 
 	private ModuleCraftingTypes()
 	{
